32c927a35c1f61d56125cc40a119d802aff4fc2e
atharva10205 <atharvapandhare3@gmail.com>
2025-06-08 02:14:54 +0530

    9

--- src/app/api/Rate_limit/route.js
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
-
-const ratelimit = new Ratelimit({
-  redis: kv,
-  limiter:Ratelimit,slidingWindow(5,'10s')
-});
